5bae84d4c98898dd47cad3ccd2267908d75d65c7
Ludwig Taeubert <ludwig.taeuber@gmail.com>
2021-07-26 21:06:22 +0200

    feat: pagination

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 use App\Entity\Property;
 use App\Repository\PropertyRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -32,10 +34,16 @@ class PropertyController extends AbstractController
      * @return Response
      */
 
-    public function index(): Response
+    public function index(PaginatorInterface $paginator, Request $request): Response
     {
+        $properties = $paginator->paginate(
+            $this->repository->findAllVisible(),
+            $request->query->getInt('page', 1),
+            12
+        );
         return $this->render('property/index.html.twig', [
             'current_menu' => 'properties',
+            'properties' => $properties,
         ]);
     }
 
