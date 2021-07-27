df5ff371d4a259c6a5e3faf1b097147471ed2350
Ludwig Taeubert <ludwig.taeuber@gmail.com>
2021-07-27 10:06:36 +0200

    feat: search

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -2,6 +2,8 @@
 namespace App\Controller;
 
 use App\Entity\Property;
+use App\Entity\PropertySearch;
+use App\Form\PropertySearchType;
 use App\Repository\PropertyRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
@@ -36,14 +38,19 @@ class PropertyController extends AbstractController
 
     public function index(PaginatorInterface $paginator, Request $request): Response
     {
+        $search = new PropertySearch();
+        $form = $this->createForm(PropertySearchType::class, $search);
+        $form->handleRequest($request);
+
         $properties = $paginator->paginate(
-            $this->repository->findAllVisible(),
+            $this->repository->findAllVisible($search),
             $request->query->getInt('page', 1),
             12
         );
         return $this->render('property/index.html.twig', [
             'current_menu' => 'properties',
             'properties' => $properties,
+            'form' => $form->createView(),
         ]);
     }
 
